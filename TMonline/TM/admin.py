c1ff22f9dbc8f6192238f1d58c764812b046c1ae
Jun gu Han <kanryu2048@unist.ac.kr>
2019-01-16 02:26:02 +0900

    update gitignore

--- TMonline/TM/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from TM.models import TM
-
-# Register your models here.
-
-class TMAdmin(admin.ModelAdmin):
-    list_display = ('content', 'last_modified')
-
-admin.site.register(TM, TMAdmin)
